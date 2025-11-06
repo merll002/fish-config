69642aea8b968fb3a54a635cf6b109f39033be16
merll002 <lcm@keemail.me>
2025-11-06 17:53:00 +0000

    Yes

---
+++ conf.d/config.fish
@@ -0,0 +1,162 @@
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+
+if status is-interactive
+
+
+
+if test (which pacman 2>/dev/null)
+    set distro "arch"
+
+else if test (which apt 2>/dev/null)
+    set distro "debian"
+
+else if test (which dnf 2>/dev/null)
+    set distro "fedora"
+
+else if test (which apk 2>/dev/null)
+    set distro "alpine"
+    
+else if test (which emerge 2>/dev/null)
+    set distro "gentoo"
+end
+
+# Organised Fish Shell Aliases
+
+## Prevent completion for my alias
+complete -e rm
+
+
+set BR (set_color --bold red)
+set BG (set_color --bold green)
+set B (set_color blue)
+set BB (set_color --bold blue)
+set BY (set_color --bold yellow)
+set RESET (set_color normal)
+
+## Wacom Stuff
+
+alias wacom='xsetwacom --set "Wacom Bamboo 2FG 4x5 Pen stylus" Button 2 pan && xsetwacom --set "Wacom Bamboo 2FG 4x5 Pen stylus"  "PanScrollThreshold" 200'
+alias enablewacom='systemctl --user stop opentabletdriver.service && sudo modprobe -i wacom && sleep 1 && xsetwacom --set "Wacom Bamboo 2FG 4x5 Pen stylus" Button 2 pan && xsetwacom --set "Wacom Bamboo 2FG 4x5 Pen stylus"  "PanScrollThreshold" 200'
+alias disablewacom='sudo modprobe -r wacom && systemctl --user start opentabletdriver.service'
+
+## Flatpak
+
+alias fr='flatpak remove'
+alias fs='flatpak search'
+alias frd='flatpak remove --delete-data'
+alias fi='flatpak install'
+alias fu='flatpak update'
+alias fl='flatpak list'
+alias spotify-update="flatpak mask --remove com.spotify.Client && flatpak update com.spotify.Client -y && flatpak mask com.spotify.Client"
+
+## File and Directory Management
+alias mkdir='mkdir -p'
+# alias rm='echo Use trash-put instead or use sudo.'
+alias cat='bat'
+alias ks='eza --icons=always --classify'
+alias sl='eza --icons=always --classify'
+alias ls='eza --icons=always --classify'
+alias la='eza --icons=always --classify -lA'
+alias tree='eza --icons=always --classify -T'
+alias fd='echo Use$BY fdf$RESET for finding files and$BY fdd$RESET for finding folders.'
+alias fdf='fzf -e --walker=file --query'
+alias fdd='fzf -e --walker=dir --query'
+alias of='xdg-open (kitten choose-files  --mode=file)'
+
+## Command Substitutions
+alias nano='micro'
+alias rlk='readlink -f'
+alias pip='uv pip'
+alias pip3='uv pip'
+
+## System Utilities
+alias dmount='udisksctl mount -b'
+alias dumount='udisksctl unmount -b'
+alias restartghostty='killall ghostty'
+alias gc='git clone -j$(nproc) --depth 1'
+alias nameof='ps -o comm= -p'
+alias fixpass='faillock --user leo --reset'
+alias gpupowersave='echo low | sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level'
+alias gpupower='echo auto | sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level'
+alias ssh='kitten ssh'
+alias df='dysk -c+uuid'
+
+## Miscellaneous
+alias aliases='micro ~/.config/fish/conf.d/config.fish'
+alias sctl='/usr/bin/systemctl'
+alias fixkvm='sudo rmmod kvm_amd && sudo rmmod kvm'
+alias getclass="/usr/lib/qt6/bin/qdbus org.kde.KWin /KWin org.kde.KWin.queryWindowInfo | grep -E 'resourceName|resourceClass'"
+alias unlock='faillock --user leo --reset'
+alias penv="if test -d .venv; else; uv venv ; end && source .venv/bin/activate.fish"
+
+## Environment Variables
+set -gx EDITOR /usr/bin/micro
+set -gx PATH $PATH:
+
+starship init fish | source
+
+zoxide init fish --cmd cd | source
+
+fzf --fish | source
+
+
+## Alternatives!
+alias unzip="alternatives ouch unzip"
+alias tar="alternatives ouch tar"
+alias readlink='alternatives rlk readlink'
+alias getpath='echo -e $BG"Use$BY rlk$BG instead."'
+
+## Package Management
+
+if string match $distro arch
+    alias i='paru -S --needed'
+    alias u='flatpak update && paru -Syu'
+    alias um='paru -Sy'
+    alias upgradethenshutdown="sudo su -c 'flatpak update -y && paru -Syu --noconfirm && shutdown now'"
+    alias r='paru -Rncs'
+    alias ss='pacman -Q | grep'
+    alias pacman='paru'
+    alias ratemirrors='rate-mirrors --disable-comments-in-file --protocol=https arch --max-delay 5200 | sudo tee /etc/pacman.d/mirrorlist && um'
+
+else if string match $distro debian
+    alias i='sudo apt install'
+    alias r='sudo apt purge --autoremove'
+    alias u='sudo apt update && sudo apt upgrade && flatpak update'
+    alias um'sudo apt update'
+    alias upgradethenshutdown="sudo su -c 'apt update && apt upgrade -y && shutdown now'"
+    alias s='apt search'
+    alias ss='apt list --installed | grep'
+    alias bat='batcat'
+    alias fd='fdfind'
+
+else if string match $distro fedora
+    alias i='sudo dnf install'
+    alias r='sudo dnf remove'
+    alias u='sudo dnf upgrade --refresh'
+    alias upgradethenshutdown="sudo su -c 'flatpak update -y && dnf upgrade --refresh && shutdown now'"
+    alias s='dnf search'
+    alias ss='dnf list --installed | grep'
+
+else if string match $distro alpine
+    alias i='sudo apk add'
+    alias r='sudo apk del'
+    alias u='sudo apk upgrade'
+    alias upgradethenshutdown="sudo su -c 'flatpak update -y && apk upgrade && shutdown now'"
+    alias s='apk search'
+end
+
+source /home/leo/.config/fish/functions/sudo.fish
+
+
+end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/leo/.lmstudio/bin
+# End of LM Studio CLI section
+
