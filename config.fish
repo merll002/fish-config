33ab625bd1632601b3f70bbbdd96526359330b78
merll002 <lcm@keemail.me>
2025-11-14 20:54:35 +0000

    CachyOS config only on cachy

--- config.fish
+++ config.fish
@@ -1,2 +1,5 @@
 #source /home/dietpi/.config/fish/conf.d/config.fish
 #source /home/dietpi/.config/fish/conf.d/uv.env.fish
+if test -d /usr/share/cachyos-fish-config
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+end
