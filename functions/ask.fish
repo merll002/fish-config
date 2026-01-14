5e1d999da4eda10bafbba0cf666d2c0deb392b93
merll002 <lcm@keemail.me>
2026-01-14 17:30:25 +0000

    actually commit ask function

---
+++ functions/ask.fish
@@ -0,0 +1,10 @@
+function ask
+    source ~/.config/fish/colours.fish
+    read search -P $BG"$argv$BY [y/N]$BB ➜$BR "
+    switch (string lower "$search")
+        case y
+            return 0
+        case '*'
+            return 1
+    end    
+end
