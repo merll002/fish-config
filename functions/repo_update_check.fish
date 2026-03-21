d7e7f975d9c83ebdb5964c667f5c565aa8b3628b
Leo <lcm@keemail.me>
2026-03-21 23:41:10 +0000

    Fix if /tmp/last-fetch doesnt exist (again)

--- functions/repo_update_check.fish
+++ functions/repo_update_check.fish
@@ -2,7 +2,9 @@ function repo_update_check
     source /home/$USER/.config/fish/colours.fish
     set repos (cat ~/.config/repos.conf)
     for repo in $repos
-        touch /tmp/last_fetch
+        if ! test -f /tmp/last_fetch
+            echo (date +%d) > /tmp/last_fetch
+        end
         if test (cat /tmp/last_fetch) != (date +%d)
             git -C "$repo" fetch &
             echo (date +%d) > /tmp/last_fetch
