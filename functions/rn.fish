2c017c549c7a34c73bfadb1d977cd5858dff1c9b
merll002 <lcm@keemail.me>
2026-03-26 13:18:56 +0000

    Update rm.fish

--- functions/rn.fish
+++ functions/rn.fish
@@ -1,9 +1,8 @@
 function rn
-    set BY (set_color --bold yellow)
-    set RESET (set_color normal)
+    source /home/$USER/.config/fish/utils.fish
+    source /home/$USER/.config/fish/colours.fish
     set newpath (string replace -rf '/[^/]+(/$|$)' '/' -- $argv[1] || echo ./)$argv[2]
-    read -P "Rename $argv[1] to $newpath?$BY [y/n] $RESET" -n 1 search
-    if string match -q (string lower "$search") "y"
-        mv $argv[1] $newpath
+    if ask "Rename $argv[1] to $newpath?"
+        mv -v $argv[1] $newpath
     end
 end
