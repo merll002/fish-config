556b3839814c944a457ed238effad66b4d1eeaf1
Leo <lcm@keemail.me>
2026-03-26 17:20:48 +0000

    Major simplify rn function

--- functions/rn.fish
+++ functions/rn.fish
@@ -1,7 +1,8 @@
 function rn
     source /home/$USER/.config/fish/utils.fish
     source /home/$USER/.config/fish/colours.fish
-    set newpath (string replace -rf '/[^/]+(/$|$)' '/' -- $argv[1] || echo ./)$argv[2]
+    set newpath (dirname $argv[1])/$argv[2]
+    # set newpath (string replace -rf '/[^/]+(/$|$)' '/' -- $argv[1] || echo ./)$argv[2]
     if ask "Rename $argv[1] to $newpath?"
         mv -v $argv[1] $newpath
     end
