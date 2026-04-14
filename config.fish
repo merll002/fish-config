3e34614059aec32f64bf47570354c66aa91733d8
merll002 <lcm@keemail.me>
2026-04-14 14:26:16 +0100

    remove config.fish because idk why its there

--- config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-source /home/$USER/.config/fish/utils.fish
-if test -d /usr/share/cachyos-fish-config
-    source /usr/share/cachyos-fish-config/cachyos-config.fish
-end
-end
