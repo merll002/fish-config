ffc12768b44863a897cb61153fe225d9363c6ab1
Leo <lcm@keemail.me>
2026-04-19 20:42:25 +0100

    fix termframe timeout and ctrl c not stopping docker compose from runnign

--- functions/docker.fish
+++ functions/docker.fish
@@ -1,7 +1,7 @@
 function docker
     if test (count $argv) = 2; and test $argv[1] = compose; and test $argv[2] = up
         wlog "Attatching interactively! (press enter) "
-        read
+        read || return 1
     end
     command docker $argv
 end
