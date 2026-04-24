fc59bdc3a8a80f71d58918da2ebcd08f9d154919
Leo <lcm@keemail.me>
2026-04-24 09:42:16 +0100

    Non-verbose depclean

--- conf.d/abbrs.fish
+++ conf.d/abbrs.fish
@@ -140,6 +140,6 @@ else if string match -q $distro gentoo
     abbr -a -- rr 'sudo emerge -ca'
     abbr -a -- s 'eix'
     abbr -a -- u 'read -P "You should sync (um) first" && sudo emerge --ask --verbose --update --deep --changed-use --with-bdeps=y @world && sudo emerge -ca'
-    abbr -a -- um 'sudo emerge --sync --quiet'
+    abbr -a -- um 'sudo EMERGE_DEFAULT_OPTS="" emerge --sync --quiet'
 end
 end
